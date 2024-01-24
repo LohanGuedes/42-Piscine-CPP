e9718921fbd57fae48caa414e3f3a8f15c2a9217
Lguedes <lohan.guedes@nutrix.tech>
2024-01-24 06:10:28 -0300

    Change human B to not be able to attack without a weapon!

--- module-01/ex03/HumanB.cpp
+++ module-01/ex03/HumanB.cpp
@@ -1,8 +1,16 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) { this->_name = name; }
+HumanB::HumanB(std::string name) {
+  this->_name = name;
+  this->_weapon = NULL;
+}
 
 void HumanB::attack() {
+
+  if (!this->_weapon) {
+    std::cout << "Oh No! not wielding a weapon!" << std::endl;
+    return;
+  }
   std::cout << this->_name << " attacks with their " << this->_weapon->getType()
             << std::endl;
 }
